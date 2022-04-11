a8d760bc67a225b6c9a61d4befaaac3ec48aa40a
seanorth <1058806659@qq.com>
2022-04-11 22:25:07 +0800

    添加原生http工具类

---
+++ src/main/java/cn/com/svf/oauthdemo/HttpUtils.java
@@ -0,0 +1,233 @@
+package cn.com.svf.oauthdemo;
+
+
+
+import org.springframework.util.StringUtils;
+
+import javax.net.ssl.*;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 原生http工具
+ *
+ * @author zhanfei
+ * @create 2022/4/11
+ */
+public class HttpUtils {
+    private final String CTYPE_FORM = "application/x-www-form-urlencoded;charset=utf-8";
+    private final String CTYPE_JSON = "application/json; charset=utf-8";
+    private final String charset = "utf-8";
+
+    private static HttpUtils instance = null;
+
+    public static HttpUtils getInstance() {
+        if (instance == null) {
+            return new HttpUtils();
+        }
+        return instance;
+    }
+
+    private class DefaultTrustManager implements X509TrustManager {
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            return null;
+        }
+
+        @Override
+        public void checkClientTrusted(X509Certificate[] chain, String authType)
+                throws CertificateException {
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] chain, String authType)
+                throws CertificateException {
+        }
+    }
+
+    /**
+     * 以application/json; charset=utf-8方式传输
+     *
+     */
+    public String postJson(String url, String jsonContent)
+            throws SocketTimeoutException, IOException {
+        return doRequest("POST", url, jsonContent, 60000, 60000, CTYPE_JSON,
+                null);
+    }
+
+    /**
+     * POST 以application/x-www-form-urlencoded;charset=utf-8方式传输
+     *
+     */
+    public String postForm(String url) throws SocketTimeoutException,
+            IOException {
+        return doRequest("POST", url, "", 60000, 60000, CTYPE_FORM, null);
+    }
+
+    /**
+     * POST 以application/x-www-form-urlencoded;charset=utf-8方式传输
+     *
+     */
+    public String postForm(String url, Map<String, String> params)
+            throws SocketTimeoutException, IOException {
+        return doRequest("POST", url, buildQuery(params), 60000, 60000,
+                CTYPE_FORM, null);
+    }
+
+    /**
+     * POST 以application/x-www-form-urlencoded;charset=utf-8方式传输
+     *
+     */
+    public String getForm(String url) throws SocketTimeoutException,
+            IOException {
+        return doRequest("GET", url, "", 60000, 60000, CTYPE_FORM, null);
+    }
+
+    /**
+     * POST 以application/x-www-form-urlencoded;charset=utf-8方式传输
+     *
+     */
+    public String getForm(String url, Map<String, String> params)
+            throws SocketTimeoutException, IOException {
+        return doRequest("GET", url, buildQuery(params), 60000, 60000,
+                CTYPE_FORM, null);
+    }
+
+
+    private String doRequest(String method, String url, String requestContent,
+                             int connectTimeout, int readTimeout, String ctype,
+                             Map<String, String> headerMap) throws SocketTimeoutException,
+            IOException {
+        HttpURLConnection conn = null;
+        OutputStream out = null;
+        String rsp = null;
+        try {
+            conn = getConnection(new URL(url), method, ctype, headerMap);
+            conn.setConnectTimeout(connectTimeout);
+            conn.setReadTimeout(readTimeout);
+
+            if(!StringUtils.isEmpty(requestContent)){
+                out = conn.getOutputStream();
+                out.write(requestContent.getBytes(charset));
+            }
+
+            rsp = getResponseAsString(conn);
+        } finally {
+            if (out != null) {
+                out.close();
+            }
+            if (conn != null) {
+                conn.disconnect();
+            }
+            conn = null;
+        }
+        return rsp;
+    }
+
+    private HttpURLConnection getConnection(URL url, String method,
+                                            String ctype, Map<String, String> headerMap) throws IOException {
+        HttpURLConnection conn;
+        if ("https".equals(url.getProtocol())) {
+            SSLContext ctx;
+            try {
+                ctx = SSLContext.getInstance("TLS");
+                ctx.init(new KeyManager[0],
+                        new TrustManager[] { new DefaultTrustManager() },
+                        new SecureRandom());
+            } catch (Exception e) {
+                throw new IOException(e);
+            }
+            HttpsURLConnection connHttps = (HttpsURLConnection) url
+                    .openConnection();
+            connHttps.setSSLSocketFactory(ctx.getSocketFactory());
+            connHttps.setHostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            });
+            conn = connHttps;
+        } else {
+            conn = (HttpURLConnection) url.openConnection();
+        }
+        conn.setRequestMethod(method);
+        conn.setDoInput(true);
+        conn.setDoOutput(true);
+        conn.setRequestProperty("Accept",
+                "text/xml,text/javascript,text/html,application/json");
+        conn.setRequestProperty("Content-Type", ctype);
+        if (headerMap != null) {
+            for (Map.Entry<String, String> entry : headerMap.entrySet()) {
+                conn.setRequestProperty(entry.getKey(), entry.getValue());
+            }
+        }
+        return conn;
+    }
+
+    private String getResponseAsString(HttpURLConnection conn)
+            throws IOException {
+        InputStream es = conn.getErrorStream();
+        if (es == null) {
+            return getStreamAsString(conn.getInputStream(), charset, conn);
+        } else {
+            String msg = getStreamAsString(es, charset, conn);
+            if (StringUtils.isEmpty(msg)) {
+                throw new IOException(conn.getResponseCode() + ":"
+                        + conn.getResponseMessage());
+            } else {
+                return msg;
+            }
+        }
+    }
+
+    private String getStreamAsString(InputStream stream, String charset,
+                                     HttpURLConnection conn) throws IOException {
+        try {
+            Reader reader = new InputStreamReader(stream, charset);
+
+            StringBuilder response = new StringBuilder();
+            final char[] buff = new char[1024];
+            int read = 0;
+            while ((read = reader.read(buff)) > 0) {
+                response.append(buff, 0, read);
+            }
+
+            return response.toString();
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+        }
+    }
+
+    private String buildQuery(Map<String, String> params) throws IOException {
+        if (params == null || params.isEmpty()) {
+            return "";
+        }
+
+        StringBuilder query = new StringBuilder();
+        Set<Map.Entry<String, String>> entries = params.entrySet();
+        boolean hasParam = false;
+
+        for (Map.Entry<String, String> entry : entries) {
+            String name = entry.getKey();
+            String value = entry.getValue();
+            if (hasParam) {
+                query.append("&");
+            } else {
+                hasParam = true;
+            }
+            query.append(name).append("=")
+                    .append(URLEncoder.encode(value, charset));
+        }
+        return query.toString();
+    }
+}
--- src/main/java/cn/com/svf/oauthdemo/IndexController.java
+++ src/main/java/cn/com/svf/oauthdemo/IndexController.java
@@ -5,6 +5,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 首页
  *
@@ -18,6 +23,20 @@ public class IndexController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(Model model) {
         model.addAttribute("recipient", "World");
+        return "index.html";
+    }
+    @RequestMapping(value = "/oauth/redirect", method = RequestMethod.GET)
+    public String redirect(Model model, HttpServletRequest httpServletRequest) throws IOException {
+        String code = httpServletRequest.getParameter("code");
+        model.addAttribute("recipient", "Redirect");
+        //后端发送请求到github请求token
+        Map<String,String> map=new HashMap<>();
+        map.put("client_id","ca0489b80247aa9e20ca");
+        map.put("client_secret","950afa7cd05c3a4eb16c7b574eca3a4c2dfad8d0");
+        map.put("code",code);
+        String tokenUrl="https://github.com/login/oauth/access_token";
+        String token = HttpUtils.getInstance().postForm(tokenUrl, map);
+        model.addAttribute("recipient", token);
         return "index.html";
     }
 }
